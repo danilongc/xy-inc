0b24cfc32d539c702ac9912725d5e42398b98a43
Danilo Nogueira Costa <danilo.costa@tqi.com.br>
2015-11-23 12:13:48 -0200

    Log4J
    Adicionando log4j

--- Sources/common-services-web/src/main/java/br/com/xyinc/commonservices/controller/RestErrorHandler.java
+++ Sources/common-services-web/src/main/java/br/com/xyinc/commonservices/controller/RestErrorHandler.java
@@ -1,7 +1,6 @@
 package br.com.xyinc.commonservices.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class RestErrorHandler {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(RestErrorHandler.class);
+	private static final Logger LOGGER = Logger.getLogger(RestErrorHandler.class);
 
 	@ExceptionHandler(AddressException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
